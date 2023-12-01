22a69945223327a27d98fc9b92750791a723d729
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-12-01 16:38:27 -0500

    admin create UI

--- src/routes/admin/products/new/+page.svelte
+++ src/routes/admin/products/new/+page.svelte
@@ -2,6 +2,10 @@
 	import { enhance } from '$app/forms';
 	import { goto } from '$app/navigation';
 	import { CldImage, CldUploadButton } from 'svelte-cloudinary';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	let images: {
 		publicId: string;
@@ -26,12 +30,26 @@
 			}
 		];
 	};
+
+	const removeImage = (idx: number) => {
+		const copy = images;
+		copy.splice(idx, 1);
+		images = copy;
+	};
 </script>
 
 <form
-	class="w-full h-full p-4 flex flex-col gap-y-3"
+	class="w-full h-full p-4 flex flex-col gap-y-6"
 	method="post"
-	use:enhance={({ formData }) => {
+	use:enhance={({ formData, cancel }) => {
+		// ensure there is at least one image
+		const images = formData.getAll('images');
+
+		if (images.length === 0) {
+			alert('must include at least one image');
+			cancel();
+		}
+
 		return ({ result }) => {
 			if (result.type === 'redirect') {
 				goto(result.location);
@@ -44,60 +62,55 @@
 	}}
 >
 	<h2 class="text-slate-900 text-3xl font-bold">Create New Product</h2>
-	<div>
-		<label for="name" class="block text-slate-800 font-light">Name</label>
-		<input
-			name="name"
-			required
-			id="name"
-			class="px-2 py-1 text-slate-900 rounded-lg bg-slate-100 w-[300px] font-light"
-			type="text"
-			placeholder="My Product"
-		/>
+	<div class="gap-1.5 grid">
+		<Label for="name">Name</Label>
+		<Input name="name" required id="name" class="w-1/3" type="text" placeholder="My Product" />
 	</div>
 
-	<div>
-		<label for="stripeProductId" class="block text-slate-800 font-light">Stripe Product Id</label>
-		<input
+	<div class="gap-1.5 grid">
+		<Label for="stripeProductId">Stripe Product Id</Label>
+		<Input
 			name="stripeProductId"
 			required
 			type="text"
 			id="stripeProductId"
-			class="px-2 py-1 text-slate-900 rounded-lg bg-slate-100 w-[300px] font-light"
 			placeholder="prod_10AF..."
+			class="w-1/3"
 		/>
+		<p class="text-sm text-muted-foreground w-1/3">
+			The id of the product you created in Stripe, make sure you get this from the dashboard!
+		</p>
 	</div>
 
-	<div>
-		<label for="stripePriceId" class="block text-slate-800 font-light">Stripe Price Id</label>
-		<input
+	<div class="gap-1.5 grid">
+		<Label for="stripePriceId">Stripe Price Id</Label>
+		<Input
 			name="stripePriceId"
 			required
 			id="stripePriceId"
-			class="px-2 py-1 text-slate-900 rounded-lg bg-slate-100 w-[300px] font-light"
+			class="w-1/3"
 			type="text"
 			placeholder="price_10AF..."
 		/>
+		<p class="text-sm text-muted-foreground w-1/3">
+			The price id, not product id of your product in Stripe. This is found lower on the page.
+		</p>
 	</div>
 
-	<div>
-		<label for="price" class="block text-slate-800 font-light">Price (number of cents)</label>
-		<input
-			name="price"
-			required
-			type="number"
-			id="price"
-			class="px-2 py-1 text-slate-900 rounded-lg bg-slate-100 w-[300px] font-light"
-			placeholder="1000"
-		/>
+	<div class="gap-1.5 grid">
+		<Label for="price">Price</Label>
+		<Input name="price" required type="number" id="price" class="w-1/3" placeholder="1000" />
+		<p class="text-sm text-muted-foreground w-1/3">
+			Enter the number of cents your product costs. This is done to stay in line with Stripe.
+		</p>
 	</div>
 
-	<div>
-		<label for="desc" class="block text-slate-800 font-light">Description</label>
-		<textarea
+	<div class="gap-1.5 grid">
+		<Label for="desc">Description</Label>
+		<Textarea
 			name="desc"
 			id="desc"
-			class="px-2 py-1 text-slate-900 rounded-lg bg-slate-100 w-[300px] font-light"
+			class="w-1/3"
 			required
 			placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 		/>
@@ -105,30 +118,35 @@
 
 	<!-- cloudinary section -->
 	<div class="flex flex-row items-center">
-		{#each images as image}
-			<div>
-				<CldImage src={image.publicId} width={356} height={200} />
+		{#each images as image, i}
+			<div class="relative w-[168px] h-[100px] rounded-lg overflow-hidden">
+				<CldImage src={image.publicId} width={168 * 2} height={100 * 2} objectFit="cover" />
+				<button
+					class="absolute w-full h-full items-center flex justify-center inset-0 bg-opacity-0 hover:bg-opacity-70 bg-gray-900 z-30 text-white text-opacity-0 hover:text-opacity-100 hover:cursor-pointer"
+					type="button"
+					on:click={() => {
+						removeImage(i);
+					}}
+				>
+					Delete
+				</button>
 				<!-- being a little fancy here creating form elements so that our form can submit without progressive enhancement... -->
-				<input value={JSON.stringify(image)} class="hidden" name="images" />
+				<input value={JSON.stringify({})} class="hidden" name="images" />
 			</div>
 		{/each}
+	</div>
+
+	<div class="w-full flex justify-start items-center gap-x-4">
 		<CldUploadButton
 			uploadPreset="admin-upload"
-			class="bg-blue-600 text-white px-4 py-1 rounded-xl"
+			class="px-4 py-2 rounded-lg border-gray-900 bg-white text-gray-900 border font-semibold hover:bg-gray-200"
 			onUpload={(res) => {
 				if (res.event === 'success') {
 					addImage(res.info);
 				}
 			}}
 		/>
-	</div>
-
-	<div class="w-full flex justify-start">
-		<button
-			type="submit"
-			class="text-white bg-blue-600 font-bold py-1 px-4 rounded-xl hover:bg-blue-500 hover:underline"
-			>Create</button
-		>
+		<Button type="submit">Create</Button>
 	</div>
 </form>
 
