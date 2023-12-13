5783dd7fa8841514b47dab7b92567149c023ef6a
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-12-13 17:47:05 -0500

    data model updates

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,13 +1,14 @@
 import { sql } from 'drizzle-orm';
-import { sqliteTable, text, integer, int } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer, int, primaryKey } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: text('id').primaryKey(),
-	first_name: text('first_name').notNull(),
-	last_name: text('last_name').notNull(),
-	is_admin: integer('is_admin', { mode: 'boolean' }).notNull(),
+	firstName: text('first_name').notNull(),
+	lastName: text('last_name').notNull(),
+	isAdmin: integer('is_admin', { mode: 'boolean' }).notNull(),
 	email: text('email').notNull().unique(),
-	hashedPassword: text('hashed_password').notNull()
+	hashedPassword: text('hashed_password').notNull(),
+	stripeCustomerId: text('stripe_customer_id').notNull()
 });
 
 export const session = sqliteTable('session', {
@@ -15,12 +16,31 @@ export const session = sqliteTable('session', {
 	userId: text('user_id')
 		.notNull()
 		.references(() => user.id),
-	expires_at: int('expires_at', {
+	expiresAt: int('expires_at', {
 		mode: 'timestamp'
 	}).notNull()
 });
 
 export const product = sqliteTable('product', {
+	id: text('id').primaryKey(),
+	name: text('name').notNull(),
+	desc: text('desc').notNull()
+});
+
+export const productToProductTag = sqliteTable(
+	'product_to_product_tag',
+	{
+		productId: text('product_id').references(() => product.id),
+		tagId: text('tag_id').references(() => productTag.id)
+	},
+	(table) => {
+		return {
+			pk: primaryKey({ columns: [table.productId, table.tagId] })
+		};
+	}
+);
+
+export const productTag = sqliteTable('product_tag', {
 	id: text('id').primaryKey(),
 	name: text('name').notNull(),
 	desc: text('desc').notNull()
@@ -38,7 +58,7 @@ export const productSize = sqliteTable('product_size', {
 export const productImage = sqliteTable('product_image', {
 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
 	cloudinaryId: text('cloudinary_id'),
-	productId: text('product_id').references(() => product.stripeProductId),
+	productId: text('product_id').references(() => product.id),
 	width: integer('width').notNull(),
 	height: integer('height').notNull()
 });
@@ -47,7 +67,8 @@ export const productReview = sqliteTable('product_review', {
 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
 	rating: integer('rating').notNull(),
 	reviewText: text('review_text'),
-	productId: text('product_id').references(() => product.stripeProductId)
+	productId: text('product_id').references(() => product.id),
+	timestamp: int('timestamp', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`)
 });
 
 export const order = sqliteTable('order', {
@@ -55,13 +76,14 @@ export const order = sqliteTable('order', {
 	stripeOrderId: text('stripe_order_id').primaryKey(),
 	stripeCustomerId: text('stripe_customer_id').notNull(),
 	totalPrice: integer('total_price').notNull(),
-	timestamp: text('timestamp').default(sql`CURRENT_TIMESTAMP`)
+	timestamp: int('timestamp', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`)
 });
 
 export const orderProduct = sqliteTable('order_product', {
 	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-	productId: text('product_id').references(() => product.stripeProductId),
+	productSizeCode: text('product_size_code').references(() => productSize.code),
 	quantity: integer('quantity'),
 	status: text('status', { enum: ['placed', 'fulfilled'] }).notNull(),
+	trackingNumber: text('tracking_number'),
 	orderId: text('order_id').references(() => order.stripeOrderId)
 });
