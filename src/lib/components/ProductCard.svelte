a860df7e89f2c07482cc23ced26cea595705f90d
Ryder Kemper <ryderkemper@MacBook-Pro-193.local>
2024-01-18 04:04:53 -0500

    mobile first products page

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -9,40 +9,83 @@
 		cloudinaryId: string | null;
 		tags: string[];
 		selectTag: (tagName: string) => void;
+		displayMode: string;
 	};
 </script>
 
-<div class="flex flex-row items-center gap-x-8 p-4 border-b">
-	<div class="w-[355px] h-[200px] flex justify-center items-center">
-		<CldImage
-			width={355 * 2}
-			height={200 * 2}
-			crop="fill"
-			src={itemData.cloudinaryId || 'https://via.placeholder.com/355x200'}
-			alt="Description of my image"
-			sizes="100vw"
-			class="rounded-lg shadow-md"
-		/>
+<div class={`flex sm:flex-row flex-col sm:items-center sm:gap-x-8 sm:gap-y-0 gap-y-2 sm:p-4 ${itemData.displayMode == 'sm' ? 'w-[47%]' : 'w-[94%]'} sm:w-full`}>
+	<div class="sm:w-[355px] w-full sm:h-[200px] flex justify-center items-center relative">
+		<div class="absolute top-1 left-1 rounded-full w-[40px] h-[40px] bg-gray-600/70 flex flex-col items-center justify-center sm:hidden">
+			<svg class="-translate-y-[1px]" width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g clip-path="url(#clip0_100_15)">
+				<path d="M0 20V4.995L1 5.001V5.016L5 5.014V4C5 1.516 6.274 0 8.5 0C10.518 0 12 1.48 12 4V5.012L17 5.009V5.994H1V19H16V6.005H17V20H0ZM11 4.49C11 2.267 10.507 1 8.5 1C6.5 1 6 2.27 6 4.49V5L11 4.998V4.49Z" fill="white"/>
+				<line x1="8.5" y1="8" x2="8.5" y2="17" stroke="white"/>
+				<line x1="4" y1="12.5" x2="13" y2="12.5" stroke="white"/>
+				</g>
+				<defs>
+				<clipPath id="clip0_100_15">
+				<rect width="17" height="20" fill="white"/>
+				</clipPath>
+				</defs>
+				</svg>
+			</div>
+		<a href={`/products/${itemData.productId}`}>
+			<CldImage
+				width={355 * 2}
+				height={200 * 2}
+				crop="fill"
+				src={itemData.cloudinaryId || 'https://via.placeholder.com/355x200'}
+				alt="Description of my image"
+				sizes="100vw"
+				class="sm:rounded-lg shadow-md hidden sm:flex"
+			/>
+			<CldImage
+				width={355 * 4}
+				height={200 * 8}
+				crop="fill"
+				src={itemData.cloudinaryId || 'https://via.placeholder.com/355x200'}
+				alt="Description of my image"
+				sizes="100vw"
+				class={`sm:rounded-lg shadow-md sm:hidden ${itemData.displayMode == 'sm' && 'hidden'}`}
+			/>
+			<CldImage
+				width={355 * 2}
+				height={200 * 4}
+				crop="fill"
+				src={itemData.cloudinaryId || 'https://via.placeholder.com/355x200'}
+				alt="Description of my image"
+				sizes="100vw"
+				class={`sm:rounded-lg shadow-md sm:hidden ${itemData.displayMode == 'lg' && 'hidden'}`}
+			/>
+		</a>
+		
 	</div>
-	<div class="grow">
-		<h2 class=" text-2xl text-neutral-200 pb-2">{itemData.name}</h2>
+	<div class="grow flex flex-col ">
+		<h2 class=" sm:text-2xl text-md font-light text-neutral-200 sm:pb-2">{itemData.name}</h2>
 		<div class="flex flex-row items-center gap-x-2">
 			{#each itemData.tags as tag}
 				<Button
 					on:click={() => itemData.selectTag(tag)}
-					class="rounded-full text-sm font-light py-1 px-2  h-auto"
+					class="rounded-full text-sm font-light py-1 px-2 hidden sm:flex h-auto"
 				>
 					{tag}
 				</Button>
+				<div class="text-sm text-gray-400 font-light py-0.5">
+					{tag} Collection
+				</div>
+				
 			{/each}
 		</div>
+		<div>
+			Sm Md Lg Xl
+		</div>
 	</div>
-	<div class="flex flex-col gap-y-2 items-center justify-center w-[175px]">
+	<div class="sm:flex flex-col hidden gap-y-2 items-center justify-center sm:w-[175px] w-full">
 		<Button class="w-full">
 			<ShoppingCart class="w-4 h-4 mr-2" />
 			Add To Cart</Button
 		>
-		<Button href={`/products/${itemData.productId}`} variant="outline" class="w-full">
+		<Button href={`/products/${itemData.productId}`} variant="outline" class="w-full ">
 			<Link class="w-4 h-4 mr-2" />
 			More Information</Button
 		>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -7,7 +7,23 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { X } from 'lucide-svelte';
-
+	// import { product } from '$lib/server/db/schema';
+	// import {
+	// 	Drawer,
+	// 	DrawerClose,
+	// 	DrawerContent,
+	// 	DrawerDescription,
+	// 	DrawerFooter,
+	// 	DrawerHeader,
+	// 	DrawerTitle,
+	// 	DrawerTrigger,
+	// } from "../../lib/components/ui/drawer"
+	import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../lib/components/ui/tabs"
 	export let data;
 
 	let searchResults: { tagName: string; tagDesc: string }[] = [];
@@ -15,6 +31,8 @@
 	let debounceTimer: NodeJS.Timeout | undefined;
 
 	let searchQuery = '';
+
+	let displayMode = 'sm'
 
 	// this feels really stupid...
 	$: searchQuery !== '' && handleInputChange(searchQuery);
@@ -85,8 +103,42 @@
 	}
 </script>
 
-<main class="grow w-full p-8 container grid grid-cols-4 gap-4">
-	<div class="h-full col-span-1">
+<main class="grow w-full sm:p-8 py-6 sm:container sm:grid sm:grid-cols-4 gap-4 flex flex-col">
+	<div class="flex flex-row w-full sm:hidden gap-2 px-2 justify-center">
+		<Tabs defaultValue="account" class="w-1/2">
+			<TabsList class="flex flex-row rounded-full py-3 bg-gray-800/80">
+			  <TabsTrigger class="rounded-full w-1/2" value="account" on:click={() => displayMode = 'sm'}>
+				<svg width="18" height="23" viewBox="0 0 18 23" fill={displayMode == 'sm' ? 'white' : 'none'} xmlns="http://www.w3.org/2000/svg">
+					<rect x="0.5" y="0.5" width="7" height="10" rx="0.5" stroke="white"/>
+					<rect x="10.5" y="0.5" width="7" height="10" rx="0.5" stroke="white"/>
+					<rect x="10.5" y="12.5" width="7" height="10" rx="0.5" stroke="white"/>
+					<rect x="0.5" y="12.5" width="7" height="10" rx="0.5" stroke="white"/>
+				</svg>
+					
+				
+					
+			  </TabsTrigger>
+			  <TabsTrigger value="password" class="rounded-full w-1/2" on:click={() => displayMode = 'lg'}>
+				<svg width="18" height="23" viewBox="0 0 18 23" fill={displayMode == 'sm' ? 'none' : 'white'} xmlns="http://www.w3.org/2000/svg">
+					<rect x="0.5" y="0.5" width="17" height="22" rx="0.5" stroke="white"/>
+				</svg>
+			  </TabsTrigger>
+			</TabsList>
+			<TabsContent value="account">
+				
+					
+			</TabsContent>
+			<TabsContent value="password">
+				
+					
+			</TabsContent>
+		</Tabs>
+		<div class="rounded-full w-1/2 h-[40px] bg-gray-800/80 flex flex-row justify-center items-center gap-3 text-md font-semibold uppercase">
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sliders-horizontal"><line x1="21" x2="14" y1="4" y2="4"/><line x1="10" x2="3" y1="4" y2="4"/><line x1="21" x2="12" y1="12" y2="12"/><line x1="8" x2="3" y1="12" y2="12"/><line x1="21" x2="16" y1="20" y2="20"/><line x1="12" x2="3" y1="20" y2="20"/><line x1="14" x2="14" y1="2" y2="6"/><line x1="8" x2="8" y1="10" y2="14"/><line x1="16" x2="16" y1="18" y2="22"/></svg>
+			Filter
+		</div>
+	</div>
+	<div class="h-full col-span-1 sm:flex hidden">
 		<div class="gap-1.5 grid relative">
 			<Label for="name">Filter by Tag</Label>
 			<Input
@@ -122,7 +174,7 @@
 			{/each}
 		</div>
 	</div>
-	<div class="flex flex-col items-left col-span-3">
+	<div class="flex sm:flex-col flex-row items-left sm:col-span-3 flex-wrap place-content-evenly">
 		{#each data.products as product}
 			<ProductCard
 				itemData={{
@@ -130,7 +182,8 @@
 					productId: product.id,
 					cloudinaryId: product.images.length > 0 ? product.images[0].cloudinaryId : null,
 					tags: product.tags.map((tag) => tag.tagId),
-					selectTag: addParam
+					selectTag: addParam,
+					displayMode: displayMode
 				}}
 			/>
 		{/each}
