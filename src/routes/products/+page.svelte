4d28d87559fdacba0c9c3b7180d778ca92a1d66b
Ryder Kemper <ryderkemper@MacBook-Pro-193.local>
2024-01-22 21:47:29 -0500

    drawer fixed

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -157,11 +157,44 @@
 			<TabsContent value="account" />
 			<TabsContent value="password" />
 		</Tabs>
-		<div
-			class="rounded-full w-1/2 h-[40px] bg-gray-800/80 flex flex-row justify-center items-center gap-3 text-md font-semibold uppercase"
-		>
+		
 		<Drawer>
-			<DrawerTrigger>Open</DrawerTrigger>
+			<DrawerTrigger class="w-1/2 p-0 h-[40px]">
+				<div
+					class="rounded-full w-full h-[40px] bg-gray-800/80 flex flex-row justify-center items-center gap-3 text-md font-semibold uppercase"
+				>
+				
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-sliders-horizontal"
+						><line x1="21" x2="14" y1="4" y2="4" /><line x1="10" x2="3" y1="4" y2="4" /><line
+							x1="21"
+							x2="12"
+							y1="12"
+							y2="12"
+						/><line x1="8" x2="3" y1="12" y2="12" /><line x1="21" x2="16" y1="20" y2="20" /><line
+							x1="12"
+							x2="3"
+							y1="20"
+							y2="20"
+						/><line x1="14" x2="14" y1="2" y2="6" /><line x1="8" x2="8" y1="10" y2="14" /><line
+							x1="16"
+							x2="16"
+							y1="18"
+							y2="22"
+						/></svg
+					>
+					Filter
+				</div>
+			</DrawerTrigger>
 			<DrawerContent>
 			  <DrawerHeader>
 				<DrawerTitle>Are you absolutely sure?</DrawerTitle>
@@ -175,36 +208,7 @@
 			  </DrawerFooter>
 			</DrawerContent>
 		  </Drawer>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="20"
-				height="20"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="lucide lucide-sliders-horizontal"
-				><line x1="21" x2="14" y1="4" y2="4" /><line x1="10" x2="3" y1="4" y2="4" /><line
-					x1="21"
-					x2="12"
-					y1="12"
-					y2="12"
-				/><line x1="8" x2="3" y1="12" y2="12" /><line x1="21" x2="16" y1="20" y2="20" /><line
-					x1="12"
-					x2="3"
-					y1="20"
-					y2="20"
-				/><line x1="14" x2="14" y1="2" y2="6" /><line x1="8" x2="8" y1="10" y2="14" /><line
-					x1="16"
-					x2="16"
-					y1="18"
-					y2="22"
-				/></svg
-			>
-			Filter
-		</div>
+		
 	</div>
 	<div class="h-full col-span-1 sm:flex hidden">
 		<div class="gap-1.5 grid relative">
