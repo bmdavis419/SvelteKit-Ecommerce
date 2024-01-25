764f23db787c94d8a897b4f85db985938c9ad32a
Ryder Kemper <ryderkemper@MacBook-Pro-193.local>
2024-01-25 12:07:07 -0500

    product page is simmering

--- src/routes/products/[productId]/+page.svelte
+++ src/routes/products/[productId]/+page.svelte
@@ -31,10 +31,10 @@
 
 <div class="grow flex flex-col sm:pt-10 w-full">
 	<!-- DESKTOP -->
-	<div class="h-full mx-10 hidden sm:grid grid-cols-4">
-		<div class="col-span-3 p-4 relative">
+		<div class="h-full mx-10 hidden sm:grid grid-cols-4">
+		<div class="col-span-3 p-4 relative h-[85vh]">
 			{#each data.product.images as im, i}
-				<CldImage src={im.cloudinaryId} width={2000} height={1300} objectFit="cover" class={`rounded-lg ${curIdx == i ? 'flex' : 'hidden'}`} />
+				<CldImage src={im.cloudinaryId} width={2000} height={1300} objectFit="cover" class={`rounded-lg h-full ${curIdx == i ? 'flex' : 'hidden'}`} />
 			{/each}
 			<div class="absolute bottom-8 left-1/2 translate-x-[-50%] flex gap-x-2">
 				{#each data.product.images as _, i}
@@ -44,13 +44,13 @@
 					/>
 				{/each}
 			</div>
-			<div class="absolute right-10 top-1/2 translate-y-[-50%] bg-gray-600/50 rounded-full flex flex-row justify-center items-center">
+			<div on:click={() => {curIdx++; if (curIdx == data.product.images.length) curIdx = 0;}} class="absolute cursor-pointer right-10 top-1/2 translate-y-[-50%] bg-gray-300/50 rounded-full flex flex-row justify-center items-center">
 				<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M8.29492 16.59L12.8749 12L8.29492 7.41L9.70492 6L15.7049 12L9.70492 18L8.29492 16.59Z" fill="black"/>
 					</svg>
 					
 			</div>
-			<div class="absolute left-10 top-1/2 translate-y-[-50%] bg-gray-600/50 rounded-full flex flex-row justify-center items-center">
+			<div on:click={() => {curIdx--; if (curIdx == -1) curIdx = data.product.images.length-1;}} class="absolute cursor-pointer left-10 top-1/2 translate-y-[-50%] bg-gray-300/50 rounded-full flex flex-row justify-center items-center">
 				<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M15.7049 16.59L11.1249 12L15.7049 7.41L14.2949 6L8.29492 12L14.2949 18L15.7049 16.59Z" fill="black"/>
 					</svg>
@@ -58,7 +58,23 @@
 			</div>
 		</div>
 		
-		<div class="p-4">
+		<div class="p-4 flex flex-col gap-6 h-[85vh] overflow-scroll no-scroll">
+			<div class="text-4xl font-jura bg-gradient-to-r from-red-600 via-purple-500 to-indigo-400 text-transparent bg-clip-text">{data.product.name}</div>
+			<div class="text-lg text-gray-500 font-light">{data.product.desc}</div>
+			<div>
+				<span class=" text-3xl font-jura bg-gradient-to-r from-red-600 via-purple-500 to-indigo-400 text-transparent bg-clip-text">Sizes.</span>
+				<span class=" text-2xl text-gray-500  font-jura">Which one do you need?</span>
+			</div>
+			
+			{#each data.product.sizes as size, i}
+				<div on:click={() => (selectedSizeIdx = i)} class={`${selectedSizeIdx == i ? 'border-blue-500 border-2' : 'border-neutral-600/50 border-[1px]'} w-full cursor-pointer p-6 bg-white rounded-lg flex flex-row justify-between items-center hover:bg-neutral-50  border-solid`}>
+					<div class="flex flex-col">
+						<div class="text-lg font-semibold">Small Square</div>
+						<div class="text-sm font-light">{size.width} x {size.height}</div>
+					</div>
+					<div>${size.price/100}</div>
+				</div>
+			{/each}
 			<Card.Root
 			class=" sm:flex hidden items-center justify-center flex-col"
 		>
@@ -235,16 +251,16 @@
 		background: black;
 		border-radius: 10px;
 	}
-	.slides::-webkit-scrollbar-track {
+	.slides::-webkit-scrollbar-track, .no-scroll::-webkit-scrollbar-track {
 		background: transparent;
 	}
 	/* Hide scrollbar for Chrome, Safari and Opera */
-.slides::-webkit-scrollbar {
+.slides::-webkit-scrollbar, .no-scroll::-webkit-scrollbar {
     display: none;
 }
 
 /* Hide scrollbar for IE, Edge and Firefox */
-.slides {
+.slides, .no-scroll {
     -ms-overflow-style: none;  /* IE and Edge */
     scrollbar-width: none;  /* Firefox */
 }
